da7647d5477a16ea53eeb70bf728e5dd21eb4fa1
OmarDes <omargiovanni39@gmail.com>
2023-05-13 18:43:50 -0500

    13 commit

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def peliculas_mes(mes:str):
     return {'mes':mes, 'cantidad':respuesta}
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', port=8000, reload=True)
+    uvicorn.run('main:app', port=10000, reload=True)
