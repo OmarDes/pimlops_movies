e41676e519dd9b41c46a61072b6ecb5d65dc03ea
OmarDes <omargiovanni39@gmail.com>
2023-05-13 18:36:31 -0500

    12 commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 
@@ -8,3 +9,6 @@ def peliculas_mes(mes:str):
     '''Se ingresa el mes y la funcion retorna la cantidad de peliculas que se estrenaron ese mes historicamente'''
     respuesta = 'hola mundo'
     return {'mes':mes, 'cantidad':respuesta}
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', port=8000, reload=True)
